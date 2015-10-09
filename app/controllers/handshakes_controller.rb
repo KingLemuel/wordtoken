8324656effd5739151430449785565e0b0b46171
kinglemuel <pro@kinglemuel.com>
2015-10-09 16:07:52 -0400

    fix redirect

--- app/controllers/handshakes_controller.rb
+++ app/controllers/handshakes_controller.rb
@@ -4,7 +4,7 @@ class HandshakesController < ApplicationController
 		@user = User.find(params[:connection_id])
 		current_user.accept_handshake @user
 		respond_to do |format|
-			format.html { redirect_to @user }
+			format.html { redirect_to current_user}
 			format.js
 		end
 	end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -8,20 +8,20 @@ class RequestsController < ApplicationController
 
 		current_user.requests.create(connection_id: @user.id)
 		respond_to do |format|
-			format.html { redirect_to @user }
+			format.html { redirect_to current_user }
 			format.js
 		end
 
 	
 	end
 
-	def destroy
-		@user = User.find(params[:connection_id])
-		current_user.cancel_request @user
-		respond_to do |format|
-			format.html { redirect_to @user }
-			format.js
-		end
-	end
+	# def destroy
+	# 	@user = User.find(params[:connection_id])
+	# 	current_user.cancel_request @user
+	# 	respond_to do |format|
+	# 		format.html { redirect_to @user }
+	# 		format.js
+	# 	end
+	# end
 
 end
