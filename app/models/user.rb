31c802297c17e2318e205bbf8554509269832ebc
kinglemuel <pro@kinglemuel.com>
2015-10-11 10:50:00 -0400

    fix credibility_given

--- app/models/user.rb
+++ app/models/user.rb
@@ -99,7 +99,7 @@ class User < ActiveRecord::Base
 	 # Credibility
 
 	 def give_credibility(other_user)
-	 	current_user_cb = CredibilitySystem.find_by(user_id: current_user.id).points
+	 	current_user_cb = CredibilitySystem.find_by(user_id: self.id).points
 	 	weight = (current_user_cb * 0.10).to_i
 	 	other_user_cb = CredibilitySystem.find_by(user_id: other_user.id).points += weight
 	 	other_user_cb.save 
