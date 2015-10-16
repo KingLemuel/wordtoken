ecfd34d8edcf6e3cea0576329fed39da4018d5ff
Umer <myemailum14@gmail.com>
2015-10-15 21:48:47 -0400

    added autocomplete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 
 Rails.application.routes.draw do
 
+  get 'autocomplete/user_json'
+
   get 'map/show'
 
   resources :users
--- public/main.js
+++ public/main.js
@@ -5,20 +5,22 @@ $(window).scroll(function() {
 var to_top = $('.to_top');
 var timer;
 
-to_top.find(".box").click(function(){
-  $('body, html').animate({ scrollTop: "0px" });
+to_top.find(".box").click(function() {
+  $('body, html').animate({
+    scrollTop: "0px"
+  });
 });
 
 function checkToTop() {
   var view = isElementInViewport(to_top);
   if (view) {
-    timer = setTimeout(function(){
-      to_top.css("transition","opacity 1s");
+    timer = setTimeout(function() {
+      to_top.css("transition", "opacity 1s");
       to_top.css('opacity', 1);
-    },300);
+    }, 300);
   } else {
     clearTimeout(timer);
-    to_top.css("transition","opacity 0s");
+    to_top.css("transition", "opacity 0s");
     to_top.css('opacity', 0);
   }
 }
@@ -40,3 +42,66 @@ function isElementInViewport(el) {
     rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
   );
 }
+
+
+
+
+
+
+// Auto complete
+(function() {
+  var input = $("#search_input_box"),
+    suggestions = $(".suggestions"),
+    no_user_msg = $(".no_user"),
+    user_temp = $('<div class="user"> <a href="#"> <div class="img"><img/></div><div class="name">Test</div></a> </div>'),
+    timer = null,
+    num = 0;
+
+
+
+  function makeAutoReq() {
+    var url = "autocomplete/user_json";
+    $.getJSON(url + '?q=' + input.val()).done(handleData);
+  }
+
+  function handleData(data) {
+    suggestions.removeClass('hide');
+    if (data.length > 0) {
+      no_user_msg.addClass('hide');
+      createUsers(data);
+    } else {
+      suggestions.find('.user').remove();
+      no_user_msg.removeClass('hide');
+    }
+  }
+
+  function createUsers(data) {
+    suggestions.find('.user').remove();
+    data.forEach(function(u) {
+      var $user = user_temp.clone();
+      $user.find('.name').text(u.name);
+      $user.find('a').attr('href',u.profile_link);
+      $user.find('img').attr('src',u.img_url);
+      suggestions.append($user);
+    });
+  }
+
+  function show_suggestions() {
+
+    clearTimeout(timer);
+  }
+
+  function blurHandler() {
+    clearTimeout(timer);
+    timer = setTimeout(function(){
+      suggestions.addClass('hide');
+    },400);
+  }
+
+
+
+  input.on('input', makeAutoReq);
+  input.on('blur', blurHandler);
+  input.on('focus', show_suggestions);
+
+})();
