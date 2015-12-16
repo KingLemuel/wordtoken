efcc69c62f5fe38ada9887b2a2ed25b494e8a784
kinglemuel <pro@kinglemuel.com>
2015-12-15 22:03:23 -0500

    update readme, add handshake spec, update user spec

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #WordToken.io
+
+As a social network based on user credibility, WordToken measures the value of each user based on a fixed and weighted point system. Similar to LinkedIn's endorsement feature, WordToken allows users to vouch for one anothers skills. These vouches are distributed via tokens, valued at 10% of the givers credibility. 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,23 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:active_connections) }
+	it { should respond_to(:passive_connections) }
+	it { should respond_to(:requests) }
+	it { should respond_to(:inverse_requests) }
+	it { should respond_to(:handshakes) }
+	it { should respond_to(:inverse_handshakes) }
+	it { should respond_to(:credibility_systems) }
+	it { should respond_to(:give_token) }
+	it { should respond_to(:ungive_token) }
+	it { should respond_to(:given?) }
+	it { should respond_to(:request_handshake) }
+	it { should respond_to(:cancel_handshake) }
+	it { should respond_to(:accept_handshake) }
+	it { should respond_to(:shook_hands?) }
+	it { should respond_to(:made_requests?) }
+	it { should respond_to(:give_credibility) }
+
 end
 
 
